d32ae52ee9b406598edc4fcf806c8c20d59364ac
Derrif2005 <Ugerardin@gmail.com>
2024-07-31 19:21:56 -0600

    A
    A

--- productos.py
+++ productos.py
@@ -11,10 +11,16 @@ def agregar_productos(codigo, nombre, marca, precio, existencias):
     }
     Inventario.append(proveedor)
 
+def buscar_productos(codigo):
+    for productos in Inventario:
+        if productos["codigo"] == codigo:
+            return productos
+    return None
+
 def eliminar_producto(codigo):
     productos=buscar_productos(codigo)
     if productos:
-        productos.remove(Inventario)
+         productos.remove(Inventario)
     return None
 
 def modificar_precios(precio):
@@ -28,11 +34,7 @@ def mostrar_productos(agregar_productos):
     print(f"codigo: {agregar_productos.codigo}")
     print(f"codigo: {agregar_productos.codigo}")
 
-def buscar_productos(codigo):
-    for productos in Inventario:
-        if productos["codigo"] == codigo:
-            return productos
-    return None
+
 
 
 
