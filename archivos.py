dfd5cda9e9b65bdb37acd010ba85f805f1fe5de1
AlejandroG <josalejandro072@gmail.com>
2024-07-31 20:22:53 -0600

    asda
    gf

--- archivos.py
+++ archivos.py
@@ -10,11 +10,12 @@ MAGENTA = "\033[35m"
 CYAN = "\033[36m"
 WHITE = "\033[37m"
 
+from datetime import datetime as dt
 import productos as pro
 #from productos import agregar_productos, buscar_productos, modificar_productos
 
 def mostrar_menu():
-    print(f" {BOLD} {BLUE} Bienvenido al sistema de gestion de estudiantes {RESET}")
+    print(f" {BOLD} {BLUE} Bienvenido al sistema de inventario {RESET}")
     print(f" {BOLD} {BLUE} Menu Principal {RESET}")
     print(f" {BOLD} {YELLOW} 1. Agregar producto {RESET}")
     print(f" {BOLD} {YELLOW} 2. Eliminar producto {RESET}")
@@ -33,7 +34,7 @@ def main():
             if opcion == "1":
                 codigo = input("Codigo: ")
                 nombre = input("Nombre: ")
-                marca =  input("Marca: ")
+                marca =  input("provedor: ")
                 precio = float(input("Precio: "))
                 existencias = int(input("Existencias: "))
                 pro.agregar_productos(codigo, nombre, marca, precio, existencias)
@@ -51,11 +52,10 @@ def main():
                 pro.modificar_precios(codigo, precio)
 
             elif opcion == "4":
-                carnet = input("Carnet: ")
-                if pro.eliminar_estudiante(carnet):
-                    print("Estudiante eliminado.")
-                else:
-                    print("Estudiante no encontrado.")
+                print(f" {BOLD} {CYAN} Listado de productos {RESET}")
+                pro.listar_producto()
+                print(f" {BOLD} {CYAN} Fecha de actualizacion: {dt.now().strftime('%Y-%m-%d %H:%M:%S')} {RESET}")
+
 
             elif opcion == "5":
                 codigo = input("Codigo: ")
@@ -86,6 +86,45 @@ def main():
         except ValueError:
             print("Opcion no valida, Intente de nuevo.")
 
+def cargarEspaciosYPrecios(archivo):
+    codigo = {}
+    nombre = {}
+    marca  = {}
+    precio = {}
+    existencias = {}
+
+    try:
+        with open(archivo, 'r') as f:
+            for linea in f: # Imprimir la línea actual
+                tipo, valor = linea.strip().split(': ') #linea.strip() elimina cualquier espacio en blanco ,#split(': ') divide la cadena en dos partes usando la cadena
+                if 'codigo' in tipo:
+                    codigo = tipo.split('')[1]
+                    codigo[codigotipo] = int(valor)
+                elif 'precios' in tipo:
+                    precio_tipo = tipo.split('')[1]
+                    precios[precio_tipo] = int(valor)
+    except FileNotFoundError:
+        print(f"El archivo {archivo} no fue encontrado.")
+    return espacios, precios
+
+def guardarFactura(pfactura):
+    opcion = input("¿En qué formato desea guardar la factura (txt o pdf)? ").lower()
+    if opcion == "txt":
+        with open("factura.txt", "w") as file:
+            file.write(pfactura)
+        print("Factura guardada en TXT")
+    elif opcion == "pdf":
+        c = canvas.Canvas("factura.pdf")
+        width, height = letter
+        c.drawString(100, height - 40, "Factura")
+        y = height - 60
+        for line in pfactura.split('\n'):
+            c.drawString(100, y, line)
+            y -= 20
+        c.save()
+        print("Factura guardada en PDF")
+    else:
+        print(f"{BOLD}{RED}Opción de formato de archivo no válida{RESET}")
+
 if __name__ == "__main__":
     main()
-
