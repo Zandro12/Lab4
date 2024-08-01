8c69d25d977d7203f08db59b7b1cf1fdac94e4b5
Derrif2005 <Ugerardin@gmail.com>
2024-07-31 19:08:04 -0600

    aaaa
    a

--- productos.py
+++ productos.py
@@ -1,34 +1,39 @@
-inventario=[]
+Inventario=[]
 
 def agregar_productos(codigo, nombre, marca, precio, existencias):
     proveedor={
-       "codigo":codigo,
-        "nombre":nombre,
-        "marca":marca,
-        "precio":precio,
-        "existencias":existencias,
-        "proveedor":[]
+       "codigo" : codigo,
+        "nombre" : nombre,
+        "marca" : marca,
+        "precio" : precio,
+        "existencias" : existencias,
+        "proveedor" : []
     }
-    proveedor.append(inventario)
+    Inventario.append(proveedor)
+
+def eliminar_producto(codigo):
+    productos=buscar_productos(codigo)
+    if productos:
+        productos.remove(Inventario)
+        return True
+    return False
+
+def modificar_precios(precio):
+    producto=buscar_productos(precio)
+    if producto:
+        producto["precio"] = precio
+        return True
+    return False
+
+def listar_productos(inventario):
+    for inventario in  :
+        .mostrar_informacion()
 
 def buscar_productos(codigo):
-    for productos in inventario:
-        if productos["codigo"]==codigo:
+    for productos in Inventario:
+        if productos["codigo"] == codigo:
             return productos
     return None
 
-def modificar_productos(carnet, nombre, edad, calificaciones):
-    estudiante=buscar_estudiante(carnet)
-    if estudiante:
-        estudiante["nombre"]=nombre
-        estudiante["edad"]=edad
-        estudiante["calificaciones"]=calificaciones
-        return True
-    return False
 
-def eliminar_estudiante(carnet):
-    estudiante=buscar_estudiante(carnet)
-    if estudiante:
-        estudiantes.remove(estudiante)
-        return True
-    return False
+
